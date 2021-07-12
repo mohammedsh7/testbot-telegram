de05557c67c401a6586bcaf312c6c36da2130279
Rasak <66588578+Artis7eeR@users.noreply.github.com>
2021-07-12 22:01:29 +0530

    fix in comment

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 const PORT = process.env.PORT || 3000;
 const URL = process.env.URL || 'https://your-heroku-app.herokuapp.com';
 
-#issue https://github.com/telegraf/telegraf/issues/44#issuecomment-269666490
+//issue https://github.com/telegraf/telegraf/issues/44#issuecomment-269666490
 bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
 bot.startWebhook(`/bot${API_TOKEN}`, null, PORT)
 
