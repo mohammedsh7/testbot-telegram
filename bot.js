db37bb0755cb1c3a9c10a2f5d2c93e4c01edfa28
Rasak <66588578+Artis7eeR@users.noreply.github.com>
2021-07-12 22:01:01 +0530

    fix port issue on launch

--- bot.js
+++ bot.js
@@ -1,13 +1,26 @@
 const { Telegraf } = require('telegraf')
-
 const bot = new Telegraf(process.env.BOT_TOKEN)
+
+const PORT = process.env.PORT || 3000;
+const URL = process.env.URL || 'https://your-heroku-app.herokuapp.com';
+
+#issue https://github.com/telegraf/telegraf/issues/44#issuecomment-269666490
+bot.telegram.setWebhook(`${URL}/bot${API_TOKEN}`);
+bot.startWebhook(`/bot${API_TOKEN}`, null, PORT)
+
+
 bot.start((ctx) => ctx.reply('Welcome'))
+
 bot.help((ctx) => ctx.reply('Send me a sticker'))
+
 bot.on('sticker', (ctx) => ctx.reply('👍 Nice'))
+
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 
 bot.command('oldschool', (ctx) => ctx.reply('Hello'))
+
 bot.command('hipster', Telegraf.reply('λ'))
+
 bot.launch()
 
 // Enable graceful stop
